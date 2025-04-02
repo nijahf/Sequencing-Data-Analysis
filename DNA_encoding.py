19eef97face8cba3b9027042d2a518f50c85c927
nijahf <152210024+nijahf@users.noreply.github.com>
2025-04-02 12:56:20 -0500

    Create DNA_encoding.py

---
+++ DNA_encoding.py
@@ -0,0 +1,12 @@
+def encodeATGC(bcdStr):
+    return int(bcdStr.replace("A","0").replace("T","1").replace("G","2").replace("C","3"), 4)
+
+
+def encodeATGC_revComp(bcdStr):
+    bcdStr = bcdStr[::-1]
+    return int(bcdStr.replace("A","1").replace("T","0").replace("G","3").replace("C","2"), 4)
+
+def decodeATGC(bcdInt, bcdLength = 12):
+    bcdString = np.base_repr(bcdInt, base = 4)
+    bcdString = ("0"*(bcdLength-len(bcdString)) ) + bcdString
+    return bcdString.replace("0","A").replace("1","T").replace("2","G").replace("3","C")
